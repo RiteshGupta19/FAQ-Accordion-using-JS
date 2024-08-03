bebdca4497ce578ee614669ac3ea975e37d9c23b
Ritessshhhh <riteshgupta0968@gmail.com>
2024-08-03 21:04:14 +0530

    Accordion

---
+++ task.js
@@ -0,0 +1,50 @@
+function one(){
+  document.getElementById("one").style.display="block";
+  document.getElementById("plus").style.display="none";
+  document.getElementById("minus").style.display="block";
+  document.getElementById("two").style.display="none";
+  document.getElementById("three").style.display="none";
+  document.getElementById("minus2").style.display="none";
+  document.getElementById("minus3").style.display="none";
+  document.getElementById("plus2").style.display="block";
+  document.getElementById("plus3").style.display="block";
+}
+function two(){
+  document.getElementById("one").style.display="none";
+  document.getElementById("plus").style.display="block";
+  document.getElementById("minus").style.display="none";
+}
+
+function one2(){
+  document.getElementById("two").style.display="block";
+  document.getElementById("plus2").style.display="none";
+  document.getElementById("minus2").style.display="block";
+  document.getElementById("one").style.display="none";
+  document.getElementById("three").style.display="none";
+  document.getElementById("plus").style.display="block";
+  document.getElementById("minus").style.display="none";
+  document.getElementById("plus3").style.display="block";
+  document.getElementById("minus3").style.display="none";
+}
+function two2(){
+  document.getElementById("two").style.display="none";
+  document.getElementById("plus2").style.display="block";
+  document.getElementById("minus2").style.display="none";
+}
+
+function one3(){
+  document.getElementById("three").style.display="block";
+  document.getElementById("plus3").style.display="none";
+  document.getElementById("minus3").style.display="block";
+  document.getElementById("one").style.display="none";
+  document.getElementById("two").style.display="none";
+  document.getElementById("plus").style.display="block";
+  document.getElementById("minus").style.display="none";
+  document.getElementById("plus2").style.display="block";
+  document.getElementById("minus2").style.display="none";
+}
+function two3(){
+  document.getElementById("three").style.display="none";
+  document.getElementById("plus3").style.display="block";
+  document.getElementById("minus3").style.display="none";
+}
